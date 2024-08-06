52dfff6be07b2440eb38998b2206ecc5aab396b2
daizharyk <aizharyk76@gmail.com>
2024-08-06 15:06:21 +0500

    8888888888888888

--- articlesService.js
+++ articlesService.js
@@ -1,17 +1,9 @@
-import {articleSection} from "./basic.js"
-
 
 import articlesData from "./article.json" with { type: "json" };
 
 
-
-
-
-
 export let articleElements = articlesData.articles.map((article) =>{
 let { name , text , id} = article;
-
-
 let articleCard = document.createElement("article");
 articleCard.className = "section_element";
 let title = document.createElement("h2");
@@ -24,38 +16,8 @@ let linkbt = document.createElement("a");
 linkbt.setAttribute("href", `article.html?articleId=${id}`);
 linkbt.textContent = "Читать далее";
 articleCard.append(linkbt);
-
 return articleCard;
 });
 
-articleSection.append(...articleElements);
-
-const params = new URLSearchParams(location.search)
-
-console.log(params);
-const articleId = params.get("articleId");
 
 
-
-const currentArticle = articlesData.articles.find((article) =>{
-  return article.id === Number(articleId)
-});
-
-console.log(currentArticle);
-
-if(currentArticle) {
-    const { name, text , id } = currentArticle;
-    let articleCard = document.createElement("article");
-    articleCard.className = "section_element";
-    let title = document.createElement("h2");
-    title.innerText = name;
-    articleCard.append(title);
-    let paragraph = document.createElement("p");
-    paragraph.innerText = text;
-    articleCard.append(paragraph);
-    let linkbt = document.createElement("a");
-linkbt.setAttribute("href", `article.html?articleId=${id}`);
-linkbt.textContent = "Читать далее";
-articleCard.append(linkbt);
-    articleSection.appendChild(articleCard);
-};
--- basic.js
+++ basic.js
@@ -1,3 +1,7 @@
+import {articleElements} from "./articlesService.js"
+import articlesData from "./article.json" with { type: "json" };
+
+
 let allArticles = document.getElementById("allarticles");
 let contacts = document.getElementById("contacts");
 
@@ -22,6 +26,46 @@ contacts.addEventListener("click" , (e)=>{
   e.preventDefault();
   scrollIntoElement(contactsSection)
 });
+
+
+
+
+
+
+articleSection.append(...articleElements);
+
+
+
+
+
+
+
+const params = new URLSearchParams(location.search)
+
+
+const articleId = params.get("articleId");
+
+const currentArticle = articlesData.articles.find((article) =>{
+  return article.id === Number(articleId)
+});
+
+
+if(currentArticle) {
+    const { name, text , id } = currentArticle;
+    let articleCard = document.createElement("article");
+    articleCard.className = "section_element";
+    let title = document.createElement("h2");
+    title.innerText = name;
+    articleCard.append(title);
+    let paragraph = document.createElement("p");
+    paragraph.innerText = text;
+    articleCard.append(paragraph);
+    let linkbt = document.createElement("a");
+linkbt.setAttribute("href", `article.html?articleId=${id}`);
+linkbt.textContent = "Читать далее";
+articleCard.append(linkbt);
+    articleSection.appendChild(articleCard);
+};
 
 
 
